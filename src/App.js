4e2c46c008a5c946e59b62e22292acfc79b570cf
Earnest Scott <earnestscott@Earnests-MacBook-Air.local>
2020-12-20 01:34:24 -0800

    deleted commented code and formatted
    Issue that took so long to fix in the last commit was
    due to the fact that I forgot setState causes a rerendering
    to occur. So with the setTimer and setInterval both calling
    setState, we were rerendering a lot! So I just made the
    setInterval time value random per component, so they're offset
    and remain consistently offset from their creation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {React, Component} from "react";
+import { React, Component } from "react";
 
 // import FadeOutLeftDiv from './FadeOut'
 import styled, { keyframes } from "styled-components";
-import {bounce} from "react-animations";
-import {fadeIn} from "react-animations";
+import { bounce } from "react-animations";
+import { fadeIn } from "react-animations";
 import Welcome from './Welcome';
 import './test.css'
 const bounceAnimation = keyframes`${bounce}`;
@@ -26,66 +26,32 @@ const FadeInDiv = styled.div`
 `;
 class App extends Component {
 
-  constructor(props) {  
-    super(props);  
-    this.state = { 
+  constructor(props) {
+    super(props);
+    this.state = {
       counter: 1,
-      // intervalId : '' 
     }
-    // this.timer=this.timer.bind(this);
 
   }
-  // componentDidMount(){
-  //   const intervalId = setInterval(
-  //     this.timer,5000
-  //   );
-  //   this.setState({intervalId : intervalId});
-  // }
-  // timer(){
-  //   const counter = this.state.counter;
-  //   this.setState({ 
-  //     counter : counter + 100
-  //   });
-    
-  // }
 
-  // componentWillUnmount(){
-  //   clearInterval(this.state.intervalId)
-  // }
-  render(){
+  render() {
     console.log(this.state.counter);
-    
-  return (
-    <div className="App">
-      <div id="toor"></div>
-      <h1>{this.state.counter}</h1>
-      <Welcome initialHor={this.state.counter} delay={5000} initialVert={200}></Welcome>
-      <Welcome initialHor={300} delay={1000} initialVert={200}></Welcome>
-      <Welcome initialHor={500} delay={3000} initialVert={200}></Welcome>
-      <div className="play">
-      <BouncyDiv>
-      <img src="https://picsum.photos/300/200/?random" />
-      </BouncyDiv>
+
+    return (
+      <div className="App">
+        <div id="toor"></div>
+        <h1>{this.state.counter}</h1>
+        <Welcome initialHor={this.state.counter} delay={5000} initialVert={200}></Welcome>
+        <Welcome initialHor={300} delay={1000} initialVert={200}></Welcome>
+        <Welcome initialHor={500} delay={3000} initialVert={200}></Welcome>
+        <div className="play">
+          <BouncyDiv>
+            <img src="https://picsum.photos/300/200/?random" />
+          </BouncyDiv>
+        </div>
+
       </div>
-      {/* <FadeInDiv>
-        <h1>Hello</h1>
-      </FadeInDiv> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
-}
+    );
+  }
 }
 export default App;
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,8 @@
-import {React, Component} from "react";
+import { React, Component } from "react";
 import fadeIn from "react-animations/lib/fade-in";
 import styled, { keyframes } from "styled-components";
 
-const getDiv = (left, top) =>{
+const getDiv = (left, top) => {
   const fadeInAnimation = keyframes`${fadeIn}`;
   const FadeInDiv = styled.div`
     animation: infinite 7s ${fadeInAnimation};
@@ -15,90 +15,50 @@ const getDiv = (left, top) =>{
 
 };
 class Welcome extends Component {
-  constructor(props) {  
-    super(props);  
-    this.state = { 
-      // counter: 1,
+  constructor(props) {
+    super(props);
+    this.state = {
       horizontal: this.props.initialHor,
       vertical: this.props.initialVert,
       intervalId: '',
-      renderTimeout: '',
-      showText: false
     };
-    this.timer=this.timer.bind(this);
+    this.timer = this.timer.bind(this);
 
   }
-   componentDidMount(){
+  componentDidMount() {
     const renderDelayBy = Math.round(Math.random() * 8000);
 
-     console.log("in component did mount");
-    // const renderDelayBy = Math.round(Math.random() * 5000);
-    //   const renderTimeout = setTimeout(() => {
-    //     console.log(`Delay component by ${renderDelayBy}`);
-    //     this.setState({
-    //       showText: true,
-    //     })
-    //   }
-    //     ,renderDelayBy);
-      const intervalId = setInterval(
-        // this.timer,5000
-        this.timer,renderDelayBy
-        );
+    const intervalId = setInterval(
+      this.timer, renderDelayBy
+    );
     this.setState({
-      intervalId : intervalId,
-      // renderTimeout: renderTimeout
+      intervalId: intervalId,
     });
   }
-   //componentDidUpdate(prevProps,prevState){
-    // if (prevState.horizontal !== this.state.horizontal || prevState.vertical !== this.state.vertical){
-    //   console.log("In here");
-    //   //const renderDelayBy = Math.round(Math.random() * 8000);
-    //   const renderDelayBy = this.props.delay;
-      
-    //   const renderTimeout = setTimeout(() => {
-    //     console.log(`Delay component by ${renderDelayBy}`);
-    //     // this.setState({
-    //     //   showText: true,
-    //     // })
-    //   }
-    //     ,renderDelayBy);
-    //     // clearInterval(await renderTimeout);
 
-    //}
-
-
-  //}
-  timer(){
-    this.setState({ 
-      horizontal : Math.round(Math.random() * 1000),
-      vertical : Math.round(Math.random() * 500)
+  timer() {
+    this.setState({
+      horizontal: Math.round(Math.random() * 1000),
+      vertical: Math.round(Math.random() * 500)
 
     });
-    
+
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.state.intervalId);
-    // clearInterval(this.state.renderTimeout);
 
   }
-  
-   render() {
-      // const renderDelayBy = Math.round(Math.random() * 50000);
-      // const renderTimeout = setTimeout(() => console.log(`Delay component by ${renderDelayBy}`),renderDelayBy);
-      // clearInterval(renderTimeout);
-      // const { showText } = this.state;
 
-      const FadeInDiv = getDiv(this.state.horizontal,this.state.vertical);
+  render() {
 
+    const FadeInDiv = getDiv(this.state.horizontal, this.state.vertical);
 
-
-      return (
-        // showText && 
-        <FadeInDiv>
-          <h1>Hello Earnest!:D</h1>
-        </FadeInDiv>
-      );
-    }
+    return (
+      <FadeInDiv>
+        <h1>Hello Earnest!:D</h1>
+      </FadeInDiv>
+    );
   }
+}
 export default Welcome;
